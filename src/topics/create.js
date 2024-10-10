4599b640cac9dce8ebd40590babf986f13f01f7b
Driftedboat <114683906+Driftedboat@users.noreply.github.com>
2024-10-11 05:50:31 +0800

    Delete isPrivate function not tied to schema

--- src/topics/create.js
+++ src/topics/create.js
@@ -39,8 +39,6 @@ module.exports = function (Topics) {
 			topicData.tags = data.tags.join(',');
 		}
 
-		topicData.isPrivate = data.privatePost !== undefined && data.privatePost; // Fixed unnecessary ternary
-
 		const result = await plugins.hooks.fire('filter:topic.create', { topic: topicData, data: data });
 		topicData = result.topic;
 		await db.setObject(`topic:${topicData.tid}`, topicData);
