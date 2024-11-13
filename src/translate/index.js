dea2a2a2a73cb5508b2f6fb49dbf5dda0119f295
Angela Kim <eunseokk@andrew.cmu.edu>
2024-11-13 16:54:42 -0500

    eslint error debugging

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,11 +1,13 @@
-var request = require('request');
+'use strict';
+
+const fetch = require('node-fetch');
 
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    // Edit the translator URL below
-    const TRANSLATOR_API = "https://translatorteambluesleep.azurewebsites.net/"
-    const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-    const data = await response.json();
-    return [data["is_english"], data["translated_content"]]
-}
+	// Edit the translator URL below
+	const TRANSLATOR_API = 'https://translatorteambluesleep.azurewebsites.net/';
+	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
+	const data = await response.json();
+	return [data.is_english, data.translated_content];
+};
