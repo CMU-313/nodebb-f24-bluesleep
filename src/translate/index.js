923d0e01d5f58f3b06b4dc8f740ac2ab3d8f4f90
Veronica Pimenova <vcpime03@gmail.com>
2024-11-15 19:32:47 -0500

    fixed lint

--- src/translate/index.js
+++ src/translate/index.js
@@ -5,9 +5,8 @@ const fetch = require('node-fetch');
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    // Update the translator URL to the correct endpoint
-    const TRANSLATOR_API = 'https://bluesleep-ai.openai.azure.com/';  // Correct endpoint
-    const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
-    const data = await response.json();
-    return [data.is_english, data.translated_content];
+	const TRANSLATOR_API = 'https://bluesleep-ai.openai.azure.com/';  // Correct endpoint
+	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
+	const data = await response.json();
+	return [data.is_english, data.translated_content];
 };
