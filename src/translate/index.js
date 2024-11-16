172912b1c7a0f402360b48e71367f930eaa9a71d
Veronica Pimenova <96704800+VeronicaPim@users.noreply.github.com>
2024-11-15 19:26:52 -0500

    updating translate url

--- src/translate/index.js
+++ src/translate/index.js
@@ -5,9 +5,9 @@ const fetch = require('node-fetch');
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-	// Edit the translator URL below
-	const TRANSLATOR_API = 'https://translatorteambluesleep.azurewebsites.net/';
-	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
-	const data = await response.json();
-	return [data.is_english, data.translated_content];
+    // Update the translator URL to the correct endpoint
+    const TRANSLATOR_API = 'https://bluesleep-ai.openai.azure.com/';  // Correct endpoint
+    const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
+    const data = await response.json();
+    return [data.is_english, data.translated_content];
 };
