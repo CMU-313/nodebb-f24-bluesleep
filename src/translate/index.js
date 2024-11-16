b72b4e55dc89c1e41cb497b73d629f0052bc984e
Veronica Pimenova <96704800+VeronicaPim@users.noreply.github.com>
2024-11-15 19:37:41 -0500

    removed comment in index js

--- src/translate/index.js
+++ src/translate/index.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch');
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-	const TRANSLATOR_API = 'https://bluesleep-ai.openai.azure.com/';  // Correct endpoint
+	const TRANSLATOR_API = 'https://bluesleep-ai.openai.azure.com/';
 	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
 	const data = await response.json();
 	return [data.is_english, data.translated_content];
